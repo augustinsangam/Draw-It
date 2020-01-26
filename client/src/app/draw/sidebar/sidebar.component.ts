e59cd4f98d4c77fcd05dfecc5ae18b0f7adfec56
Issam Maghni <me@concati.me>
2020-01-26 14:09:26 -0500

    Selection d’outils

--- client/src/app/draw/sidebar/sidebar.component.ts
+++ client/src/app/draw/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+
+import { Tool } from '../tool/tool.enum';
 
 @Component({
   selector: 'draw-sidebar',
@@ -6,8 +8,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
-  @Output() sharedEvents = new EventEmitter<>();
+  @Output() sharedEvents = new EventEmitter<Tool>();
+  private selectedTool: Tool;
+  private selectedElDOMTokenList: DOMTokenList;
 
   constructor() {
+  }
+
+  selectTool(tool: Tool, {classList}: HTMLElement) {
+    if (this.selectedTool != tool) {
+      this.selectedTool = tool;
+      // TODO: TypeScript 3.7: this.selectedElDOMTokenList?.remove(…)
+      if (!!this.selectedElDOMTokenList) {
+        this.selectedElDOMTokenList.remove('selected');
+      }
+      classList.add('selected');
+      this.selectedElDOMTokenList = classList;
+    }
+  }
+
+  selectPencil({target}: MouseEvent) {
+    this.selectTool(Tool.Pencil, target as HTMLElement);
+  }
+
+  selectBrush({target}: MouseEvent) {
+    this.selectTool(Tool.Brush, target as HTMLElement);
+  }
+
+  selectEraser({target}: MouseEvent) {
+    this.selectTool(Tool.Eraser, target as HTMLElement);
   }
 }
---
+++ client/src/app/draw/tool/tool.enum.ts
@@ -0,0 +1,6 @@
+export enum Tool {
+  Brush,
+  Pencil,
+  //
+  Eraser,
+}
