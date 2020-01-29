9696ed91efb33676817db514fd6e6fffe46bd625
Eya-Tom Augustin SANGAM <eya-tom-augustin.sangam@polymtl.ca>
2020-01-29 10:42:46 -0500

    Avancement sur le panneau lateral de la ligne

--- client/src/app/tool/pencil/pencil-panel/pencil-panel.component.ts
+++ client/src/app/tool/pencil/pencil-panel/pencil-panel.component.ts
@@ -3,6 +3,11 @@ import { Component } from '@angular/core';
 import { ToolPanelComponent } from '../../tool-panel/tool-panel.component';
 import { PencilService } from '../pencil.service';
 
+enum JonctionOptions {
+  EnableJonction = 'AVEC',
+  DisableJonction = 'SANS',
+}
+
 @Component({
   selector: 'app-pencil-panel',
   templateUrl: './pencil-panel.component.html',
@@ -10,8 +15,12 @@ import { PencilService } from '../pencil.service';
 })
 export class PencilPanelComponent extends ToolPanelComponent {
 
+  jonctionOption  = JonctionOptions.EnableJonction;
+  jonctionOptions = [JonctionOptions.EnableJonction, JonctionOptions.DisableJonction];
+
   constructor(private readonly service: PencilService) {
     super();
     console.log(this.service);
   }
+
 }
