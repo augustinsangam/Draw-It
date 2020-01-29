7496b257f4f3f060f2beda73d8b22499feda5780
Eya-Tom Augustin SANGAM <eya-tom-augustin.sangam@polymtl.ca>
2020-01-29 17:33:52 -0500

    Panneau de la ligne prêt

--- client/src/app/tool/pencil/pencil-panel/pencil-panel.component.ts
+++ client/src/app/tool/pencil/pencil-panel/pencil-panel.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatRadioChange } from '@angular/material';
 
 import { ToolPanelComponent } from '../../tool-panel/tool-panel.component';
 import { PencilService } from '../pencil.service';
-
-enum JonctionOptions {
-  EnableJonction = 'AVEC',
-  DisableJonction = 'SANS',
-}
 
 @Component({
   selector: 'app-pencil-panel',
@@ -15,12 +12,37 @@ enum JonctionOptions {
 })
 export class PencilPanelComponent extends ToolPanelComponent {
 
-  jonctionOption  = JonctionOptions.EnableJonction;
-  jonctionOptions = [JonctionOptions.EnableJonction, JonctionOptions.DisableJonction];
+  jonctionOption  = this.service.jonctionOption;
+  jonctionOptions = this.service.jonctionOptions;
 
-  constructor(private readonly service: PencilService) {
+  pencilForm: FormGroup;
+
+  constructor(private readonly service: PencilService,
+              private formBuilder: FormBuilder) {
     super();
     console.log(this.service);
+    this.pencilForm = this.formBuilder.group({
+      thicknessFormField: [this.service.thickness, [Validators.required]],
+      diameterFormField: [this.service.diameter, [Validators.required]],
+      jonctionOption: [this.service.jonctionOption, []],
+      thicknessSlider: [this.service.thickness, []],
+      diameterSlider: [this.service.diameter, []],
+    });
+  }
+
+  onChangeJonctionOption($event: MatRadioChange) {
+    this.jonctionOption = $event.value;
+    this.service.jonctionOption = ($event.value);
+  }
+
+  onThicknessChange($event: Event) {
+    this.pencilForm.patchValue({thicknessFormField : this.pencilForm.value.thicknessSlider});
+    this.service.thickness = this.pencilForm.value.thicknessSlider;
+  }
+
+  onDiameterChange($event: Event) {
+    this.pencilForm.patchValue({diameterFormField : this.pencilForm.value.diameterSlider});
+    this.service.diameter = this.pencilForm.value.diameterSlider;
   }
 
 }
