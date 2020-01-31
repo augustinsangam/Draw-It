fa1f5a10f7023d13937f15a012bed51681908261
Mathurin <mathurin.chritin@polymtl.ca>
2020-01-31 08:39:58 -0500

    le new draw utilise les memes couleurs prédéfinies que l'outil palette de couleur

--- client/src/app/pages/new-draw/new-draw.component.ts
+++ client/src/app/pages/new-draw/new-draw.component.ts
@@ -18,18 +18,8 @@ export interface DialogData {
 export class NewDrawComponent implements OnInit, AfterViewInit, OnDestroy {
 
   startColor = '#FFFFFF';
-  baseColors = [
-    '#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
-    '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
-    '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',
-    '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
-    '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC',
-    '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
-    '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680',
-    '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
-    '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3',
-    '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF',
-  ];
+  baseColors = ['#000000', '#FFFFFF', '#FF0000', '#00FF00', '#0000FF',
+    '#FFFF00', '#FF00FF', '#00FFFF', '#FF6600', '#FF6699'];
   form: FormGroup;
   maxWidth: number;
   maxHeight: number;
