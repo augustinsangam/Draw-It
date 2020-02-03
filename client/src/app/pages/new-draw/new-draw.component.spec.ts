c7c57da04bd23db75b76fd479cc6cd81327338fc
Hubert Grootenboer <hubert.grootenboer@polymtl.ca>
2020-02-03 18:01:41 -0500

    Test du nouveau dessin

--- client/src/app/pages/new-draw/new-draw.component.spec.ts
+++ client/src/app/pages/new-draw/new-draw.component.spec.ts
@@ -1,14 +1,40 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NewDrawComponent } from './new-draw.component';
 
-describe('NewDrawComponent', () => {
+import { CdkObserveContent } from '@angular/cdk/observers';
+import { Overlay } from '@angular/cdk/overlay';
+import { MatDialog, MatFormField, MatHint, MatLabel, MAT_DIALOG_SCROLL_STRATEGY_PROVIDER, MatDialogRef } from '@angular/material';
+import { ColorPicklerItemComponent } from 'src/app/tool/color/color-panel/color-pickler-item/color-pickler-item.component';
+import { ScreenService } from './sreen-service/screen.service';
+
+fdescribe('NewDrawComponent', () => {
   let component: NewDrawComponent;
   let fixture: ComponentFixture<NewDrawComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NewDrawComponent ]
+      imports: [
+        FormsModule,
+        ReactiveFormsModule
+      ],
+      declarations: [
+        NewDrawComponent,
+        ColorPicklerItemComponent,
+        MatLabel,
+        MatHint,
+        MatFormField,
+        CdkObserveContent
+      ],
+      providers: [
+        ScreenService,
+        FormBuilder,
+        MatDialog,
+        Overlay,
+        MAT_DIALOG_SCROLL_STRATEGY_PROVIDER,
+        MatDialogRef
+      ]
     })
     .compileComponents();
   }));
