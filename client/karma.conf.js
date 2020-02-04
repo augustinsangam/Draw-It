e914f5e240fabddc34683a9b4d0212f69770ffc2
Eya-Tom Augustin SANGAM <eya-tom-augustin.sangam@polymtl.ca>
2020-02-04 22:19:35 +0000

    Update client/karma.conf.js

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function(config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        singleRun: false,
+        singleRun: true,
         browsers: ['Chrome', 'ChromeHeadlessNoSandbox', 'Firefox', 'FirefoxHeadless'],
         customLaunchers: {
             FirefoxHeadless: {
