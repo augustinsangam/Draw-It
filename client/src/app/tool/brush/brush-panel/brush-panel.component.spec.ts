be14888fde581546cd267f9da5d253dbc3dc0931
Mathurin <mathurin.chritin@polymtl.ca>
2020-02-05 09:32:12 -0500

    brush-panel service de nouveau private

--- client/src/app/tool/brush/brush-panel/brush-panel.component.spec.ts
+++ client/src/app/tool/brush/brush-panel/brush-panel.component.spec.ts
@@ -69,7 +69,7 @@ describe('BrushPanelComponent', () => {
 
   it('onOptionChange devrait changer la valeur de la texture du service', () => {
     component.onOptionChange(new MatRadioChange(component.radioChoice, 'filter2'));
-    expect(component.service.texture).toEqual('filter2');
+    expect(component["service"].texture).toEqual('filter2');
   })
 
 });
