9df5c88b5177a0d3e4ab9a25dddecccf8896f22c
Issam Maghni <issam.maghni@polymtl.ca>
2020-02-07 17:01:08 -0500

    Tests des événements de la ligne

--- client/src/app/tool/line/line-logic/line-logic.component.ts
+++ client/src/app/tool/line/line-logic/line-logic.component.ts
@@ -1,4 +1,4 @@
-import { Component, Renderer2 } from '@angular/core';
+import { Component, OnDestroy, Renderer2 } from '@angular/core';
 import { ColorService } from '../../color/color.service';
 import { MathService} from '../../mathematicService/tool.math-service.service'
 import { Point } from '../../tool-common classes/Point'
@@ -11,56 +11,46 @@ import { Path } from './Path'
   selector: 'app-line-logic',
   template: ''
 })
-export class LineLogicComponent extends ToolLogicComponent {
-  private paths: Path[] = [];
-  private isNewPath = true;
+export class LineLogicComponent extends ToolLogicComponent
+                                implements OnDestroy {
+  private paths: Path[];
+  private listeners: (() => void)[];
+  private isNewPath: boolean;
   private mousePosition: Point;
-  private mathService = new MathService();
   private currentJonctionOptions: JonctionOption;
+
   constructor(private readonly service: LineService,
               private readonly renderer: Renderer2,
-              private readonly serviceColor: ColorService) {
+              private readonly serviceColor: ColorService,
+              private readonly mathService: MathService) {
     super();
+    this.paths = new Array();
+    this.listeners = new Array();
+    this.isNewPath = true;
   }
-  private listeners: (() => void)[] = [];
 
   // tslint:disable-next-line use-lifecycle-interface
   ngOnInit() {
-    const onMouseDown = this.renderer.listen(this.svgElRef.nativeElement, 'click', (mouseEv: MouseEvent) => {
-      this.onMouseClick(mouseEv);
-    });
+    this.listeners.push(this.renderer.listen(this.svgElRef.nativeElement,
+      'click', (mouseEv: MouseEvent) => this.onMouseClick(mouseEv)));
 
-    const onMouseMove = this.renderer.listen(this.svgElRef.nativeElement, 'mousemove', (mouseEv: MouseEvent) => {
-      this.onMouseMove(mouseEv);
-    });
-    const onMouseUp = this.renderer.listen(this.svgElRef.nativeElement, 'dblclick', (mouseEv: MouseEvent) => {
-      this.onMouseUp(mouseEv);
-    });
-    const onKeyDown = this.renderer.listen('document', 'keydown', (keyEv: KeyboardEvent) => {
-      this.onKeyDown(keyEv);
-    });
-    const onKeyUp = this.renderer.listen('document', 'keyup', (keyEv: KeyboardEvent) => {
-      this.onKeyUp(keyEv);
-    });
-    this.listeners = [onMouseDown, onMouseMove, onMouseUp, onKeyUp, onKeyDown];
+    this.listeners.push(this.renderer.listen(this.svgElRef.nativeElement,
+      'dblclick', (mouseEv: MouseEvent) => this.onMouseDblClick(mouseEv)));
+
+    this.listeners.push(this.renderer.listen(this.svgElRef.nativeElement,
+      'mousemove', (mouseEv: MouseEvent) => this.onMouseMove(mouseEv)));
+
+    this.listeners.push(this.renderer.listen(this.svgElRef.nativeElement,
+      'keydown', (keyEv: KeyboardEvent) => this.onKeyDown(keyEv)));
+
+    this.listeners.push(this.renderer.listen(this.svgElRef.nativeElement,
+      'keyup', (keyEv: KeyboardEvent) => this.onKeyUp(keyEv)));
   }
-  createNewPath(initialPoint: Point) {
-    const path = this.renderer.createElement('path', this.svgNS);
-    this.renderer.appendChild(this.svgElRef.nativeElement, path);
-    this.paths.push(new Path(initialPoint, this.renderer, path, this.service.withJonction));
-    this.getPath().setLineCss(this.service.thickness.toString(), this.serviceColor.primaryColor);
+
+  ngOnDestroy() {
+    this.listeners.forEach(listenner => listenner())
   }
-  createJonction(center: Point) {
-    const circle = this.renderer.createElement('circle', this.svgNS);
-    this.renderer.appendChild(this.svgElRef.nativeElement, circle);
-    this.getPath().addJonction(circle, center, this.currentJonctionOptions.radius, this.currentJonctionOptions.color);
-  }
-  addNewLine(currentPoint: Point) {
-    this.getPath().addLine(currentPoint);
-    if (this.getPath().withJonctions) {
-      this.createJonction(currentPoint);
-    }
-  }
+
   onMouseClick(mouseEv: MouseEvent) {
     let currentPoint = {x: mouseEv.offsetX, y: mouseEv.offsetY};
     if (this.isNewPath) {
@@ -74,21 +64,14 @@ export class LineLogicComponent extends ToolLogicComponent {
     }
     this.addNewLine(currentPoint)
   }
-  onMouseMove(mouseEv: MouseEvent) {
-    if (!this.isNewPath) {
-      let point = this.mousePosition = {x: mouseEv.offsetX, y: mouseEv.offsetY};
-      if (mouseEv.shiftKey) {
-        point = this.getPath().getAlignedPoint(point)
-      }
-      this.getPath().simulateNewLine(point);
-    }
-  }
-  onMouseUp(mouseEv: MouseEvent) {
+
+  onMouseDblClick(mouseEv: MouseEvent) {
     if (!this.isNewPath) {
       let currentPoint = {x: mouseEv.offsetX, y: mouseEv.offsetY};
       this.getPath().removeLastLine(); // cancel the click event
       this.getPath().removeLastLine();
-      const isLessThan3pixels = this.mathService.distanceIsLessThan3Pixel(currentPoint, this.getPath().datas.points[0])
+      const isLessThan3pixels = this.mathService.distanceIsLessThan3Pixel(
+        currentPoint, this.getPath().datas.points[0])
       if (isLessThan3pixels) {
         this.getPath().closePath();
       } else {
@@ -100,6 +83,17 @@ export class LineLogicComponent extends ToolLogicComponent {
       this.isNewPath = true;
     }
   }
+
+  onMouseMove(mouseEv: MouseEvent) {
+    if (!this.isNewPath) {
+      let point = this.mousePosition = {x: mouseEv.offsetX, y: mouseEv.offsetY};
+      if (mouseEv.shiftKey) {
+        point = this.getPath().getAlignedPoint(point)
+      }
+      this.getPath().simulateNewLine(point);
+    }
+  }
+
   onKeyDown(keyEv: KeyboardEvent) {
     const shiftIsPressed = (keyEv.code === 'ShiftLeft' || keyEv.code === 'ShiftRight')
     if (keyEv.code === 'Escape' && !this.isNewPath) {
@@ -115,19 +109,35 @@ export class LineLogicComponent extends ToolLogicComponent {
       this.getPath().simulateNewLine(transformedPoint);
     }
   }
+
   onKeyUp(keyEv: KeyboardEvent) {
     const shiftIsPressed = (keyEv.code === 'ShiftLeft' || keyEv.code === 'ShiftRight')
     if (shiftIsPressed && !this.isNewPath) {
       this.getPath().simulateNewLine(this.mousePosition);
     }
   }
+
+  createNewPath(initialPoint: Point) {
+    const path = this.renderer.createElement('path', this.svgNS);
+    this.renderer.appendChild(this.svgElRef.nativeElement, path);
+    this.paths.push(new Path(initialPoint, this.renderer, path, this.service.withJonction));
+    this.getPath().setLineCss(this.service.thickness.toString(), this.serviceColor.primaryColor);
+  }
+
+  createJonction(center: Point) {
+    const circle = this.renderer.createElement('circle', this.svgNS);
+    this.renderer.appendChild(this.svgElRef.nativeElement, circle);
+    this.getPath().addJonction(circle, center, this.currentJonctionOptions.radius, this.currentJonctionOptions.color);
+  }
+
+  addNewLine(currentPoint: Point) {
+    this.getPath().addLine(currentPoint);
+    if (this.getPath().withJonctions) {
+      this.createJonction(currentPoint);
+    }
+  }
+
   getPath(): Path {
     return this.paths[this.paths.length - 1];
-  }
-  // tslint:disable-next-line:use-lifecycle-interface
-  ngOnDestroy() {
-    this.listeners.forEach(listenner => {
-      listenner();
-    })
   }
 }
