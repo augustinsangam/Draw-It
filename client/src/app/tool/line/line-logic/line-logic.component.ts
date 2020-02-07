dab03b367af8e45ae070d21d39a14fa01db8d429
Hubert Grootenboer <hubert.grootenboer@polymtl.ca>
2020-02-07 15:49:45 -0800

    Fix des erreurs et conflits

--- client/src/app/tool/line/line-logic/line-logic.component.ts
+++ client/src/app/tool/line/line-logic/line-logic.component.ts
@@ -1,12 +1,7 @@
 import { Component, OnDestroy, Renderer2 } from '@angular/core';
 import { ColorService } from '../../color/color.service';
-<<<<<<< HEAD
-import { MathService} from '../../mathematics/tool.math-service.service'
-import { Point } from '../../tool-common classes/Point'
-=======
 import { Point } from '../../common/Point'
-import { MathService} from '../../mathematicService/tool.math-service.service'
->>>>>>> next
+import { MathService} from '../../mathematics/tool.math-service.service'
 import { ToolLogicDirective } from '../../tool-logic/tool-logic.directive';
 import { LineService } from '../line.service';
 import { JonctionOption } from './jonctionOptions'
