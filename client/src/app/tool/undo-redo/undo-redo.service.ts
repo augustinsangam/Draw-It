647fe7bd0a8de49a7806d565b36ccdddc561eb62
nicolas <nicolas.verbaere@polymtl.ca>
2020-02-18 13:24:59 -0800

    fonction de sauvegarde du svg pour le undo-redo implementer

--- client/src/app/tool/undo-redo/undo-redo.service.ts
+++ client/src/app/tool/undo-redo/undo-redo.service.ts
@@ -4,31 +4,31 @@ import { ElementRef, Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class UndoRedoService {
-  private cmdDone: HTMLElement [];
-  private cmdUndone: HTMLElement [];
+  private cmdDone: SVGSVGElement [];
+  private cmdUndone: SVGSVGElement  [];
   private svgElRef: ElementRef<SVGSVGElement>;
   constructor(svgElRef: ElementRef<SVGSVGElement>) {
     this.svgElRef = svgElRef;
     this.cmdDone = [];
     this.cmdUndone = [];
   }
-  addToCmdDone(cmd: HTMLElement): void {
+  addToCmdDone(cmd: SVGSVGElement ): void {
     this.cmdDone.push(cmd);
     this.cmdUndone = [];
   }
   undo(): void {
     if (this.cmdDone.length) {
-      this.cmdUndone.push(this.cmdDone.pop() as HTMLElement);
+      this.cmdUndone.push(this.cmdDone.pop() as SVGSVGElement );
       this.refresh(this.cmdDone[this.cmdDone.length - 1])
     }
   }
   redo(): void {
     if (this.cmdUndone.length) {
-      this.cmdDone.push(this.cmdUndone.pop() as HTMLElement);
+      this.cmdDone.push(this.cmdUndone.pop() as SVGSVGElement );
       this.refresh(this.cmdUndone[this.cmdUndone.length - 1])
     }
   }
-  refresh(svgNativeElement: HTMLElement): void {
+  refresh(svgNativeElement: SVGSVGElement ): void {
     const svg = svgNativeElement.cloneNode();
     this.svgElRef.nativeElement.replaceWith(svg);
   }
