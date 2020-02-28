0c95ba4bbe81d564f96c8af5984442bb184cbaf7
Eya-Tom Augustin SANGAM <eya-tom-augustin.sangam@polymtl.ca>
2020-02-28 11:15:06 -0500

    Save avec Angular Material

--- client/src/app/pages/save/save.component.ts
+++ client/src/app/pages/save/save.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit, ElementRef, ViewChild, Renderer2 } from '@angular/core';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { Component, ElementRef, OnInit, Optional, Renderer2, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatChipInputEvent, MatDialogRef } from '@angular/material';
 import { CommunicationService } from 'src/app/communication/communication.service';
-import { MatDialogRef } from '@angular/material';
 import { SvgService } from 'src/app/svg/svg.service';
-import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-save',
@@ -10,60 +11,66 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./save.component.scss']
 })
 export class SaveComponent implements OnInit {
+
   @ViewChild('preview', {
     static: true,
-  })
-  private readonly elementRef: ElementRef<SVGSVGElement>;
+  }) private readonly elementRef: ElementRef<SVGSVGElement>;
 
-  // Must be public
-  saving: boolean;
-  // Must be public
-  readonly tags: Set<string>;
-
+  protected form: FormGroup;
+  protected addOnBlur: boolean;
+  protected tags: string[] = ['Mes dessins'];
+  readonly separatorKeysCodes: number[];
   private gElOffset?: flatbuffers.Offset;
+  private saving: boolean;
 
-  constructor(
-    private readonly communicationService: CommunicationService,
-    readonly dialogRef: MatDialogRef<SaveComponent>,
-    private readonly renderer: Renderer2,
-    private svgService: SvgService,
-  ) {
+  constructor(private formBuilder: FormBuilder,
+              private svgService: SvgService,
+              private renderer: Renderer2,
+              private communicationService: CommunicationService,
+              @Optional() private dialogRef: MatDialogRef<SaveComponent>) {
     this.saving = false;
-    this.tags = new Set(svgService.tags);
+    this.addOnBlur = true;
+    this.separatorKeysCodes = [ENTER, COMMA];
+    this.form = this.formBuilder.group({
+      name: [, [Validators.required, Validators.pattern('.{3,21}')]]
+    });
   }
-
 
   ngOnInit(): void {
     const clone = this.svgService.structure.drawZone.cloneNode(true);
     const h = this.svgService.shape.height;
     const w = this.svgService.shape.width;
     this.elementRef.nativeElement.setAttribute('viewBox', `0 0 ${w} ${h}`);
+    this.elementRef.nativeElement.style.backgroundColor = this.svgService.shape.color;
     this.renderer.appendChild(this.elementRef.nativeElement, clone);
-
     this.communicationService.clear();
     this.gElOffset = this.communicationService
       .encodeElementRecursively(clone as Element);
   }
 
-  // angular.io/guide/user-input#type-the-event
-  // Must be public
-  add(keyEv: KeyboardEvent) {
-    const inputEl = keyEv.target as HTMLInputElement;
-    if (inputEl.checkValidity()) {
-      // Do not submit
-      keyEv.preventDefault();
-      this.tags.add(inputEl.value);
-      inputEl.value = '';
+  add(event: MatChipInputEvent): void {
+    const input = event.input;
+    const value = event.value;
+    const toAdd = (value || '').trim();
+    if (toAdd.length >= 3 && toAdd.length <= 21) {
+      this.tags.push(value.trim());
+      input.value = '';
     }
   }
 
-  // Must be public
-  submit(f: NgForm): void {
+  remove(tag: string): void {
+    const index = this.tags.indexOf(tag);
+    if (index >= 0) {
+      this.tags.splice(index, 1);
+    }
+  }
+
+  onSubmit(): void {
     if (this.gElOffset == null) {
       return;
     }
     this.saving = true;
-    this.svgService.name = f.value.name;
+    this.svgService.name = this.form.controls.name.value;
     this.svgService.tags = Array.from(this.tags);
     this.communicationService.encode(
       this.svgService.name,
@@ -85,6 +92,10 @@ export class SaveComponent implements OnInit {
         })
         .catch((err) => this.dialogRef.close(err));
     }
+  }
+
+  onCancel(): void {
+    this.dialogRef.close('Opération annulée');
   }
 
 }
