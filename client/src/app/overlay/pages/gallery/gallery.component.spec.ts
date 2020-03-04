2743b21cc995253ba2d9ef1809e853dfb15bca31
Hubert Grootenboer <hubert.grootenboer@polymtl.ca>
2020-03-04 13:55:57 -0500

    Le test bed de gallery-component fonctionne

--- client/src/app/overlay/pages/gallery/gallery.component.spec.ts
+++ client/src/app/overlay/pages/gallery/gallery.component.spec.ts
@@ -1,14 +1,39 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GalleryComponent } from './gallery.component';
+import { MaterialModule } from 'src/app/material.module';
+import { TagsFilterComponent } from './tags-filter/tags-filter.component';
+import { GalleryCardComponent } from './gallery-card/gallery-card.component';
+import { DeleteConfirmationDialogComponent } from './deleteconfirmation-dialog.component';
+import { Overlay } from '@angular/cdk/overlay';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
-describe('GalleryComponent', () => {
+fdescribe('GalleryComponent', () => {
   let component: GalleryComponent;
   let fixture: ComponentFixture<GalleryComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GalleryComponent ]
+      declarations: [
+        DeleteConfirmationDialogComponent,
+        GalleryCardComponent,
+        GalleryComponent,
+        TagsFilterComponent,
+      ],
+      imports: [
+        MaterialModule,
+        ReactiveFormsModule,
+      ],
+      providers: [
+        FormBuilder,
+        MatDialog,
+        Overlay,
+        {
+          provide: MAT_DIALOG_DATA,
+          useValue: {}
+        },
+      ]
     })
     .compileComponents();
   }));
