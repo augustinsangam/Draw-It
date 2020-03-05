8f8dd03bac64e6d3d0a98ae34b17e4318caa030c
Hubert Grootenboer <hubert.grootenboer@polymtl.ca>
2020-03-05 18:26:12 -0500

    coverage de gallery-card a 100%

--- client/src/app/overlay/pages/gallery/gallery-card/gallery-card.component.spec.ts
+++ client/src/app/overlay/pages/gallery/gallery-card/gallery-card.component.spec.ts
@@ -3,6 +3,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MaterialModule } from 'src/app/material.module';
 import { GalleryCardComponent } from './gallery-card.component';
 
+// tslint:disable: no-magic-numbers
+
 fdescribe('GalleryCardComponent', () => {
   let component: GalleryCardComponent;
   let fixture: ComponentFixture<GalleryCardComponent>;
@@ -41,5 +43,47 @@ fdescribe('GalleryCardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('#ngAfterViewInit should set the width of svgRef.nativeElement to 300 if it is the most limiting dimension', () => {
+    component.draw.shape.width = 400;
+    component.draw.shape.height = 100;
+
+    component.ngAfterViewInit();
+
+    expect(component.svgRef.nativeElement.clientWidth).toEqual(300);
+  });
+
+  it('#ngAfterViewInit should set the height of svgRef.nativeElement to 170 if it is the most limiting dimension', () => {
+    component.draw.shape.width = 300;
+    component.draw.shape.height = 200;
+
+    component.ngAfterViewInit();
+
+    expect(component.svgRef.nativeElement.clientHeight).toEqual(170);
+  });
+
+  it('#onLoad should call load.next()', () => {
+    const spy = spyOn(component.load, 'next');
+
+    component.onLoad();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('#onDelete should call delete.next()', () => {
+    const spy = spyOn(component.delete, 'next');
+
+    component.onDelete();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('#onClick should call tagClick.next() with "tag"', () => {
+    const spy = spyOn(component.tagClick, 'next');
+
+    component.onClick('test1');
+
+    expect(spy).toHaveBeenCalledWith('test1');
   });
 });
