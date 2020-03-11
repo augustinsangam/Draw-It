3678318a9b78e4bd7f63415ae7554998786daa0c
Mathurin Chritin <mathurin.chritin@polymtl.ca>
2020-03-10 20:31:39 -0400

    avancement tests grid

--- client/src/app/tool/grid/grid-panel/grid-panel.component.spec.ts
+++ client/src/app/tool/grid/grid-panel/grid-panel.component.spec.ts
@@ -1,14 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { MaterialModule } from '../../../material.module';
 
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {ColorPanelComponent} from '../../color/color-panel/color-panel.component';
+import {ColorPickerContentComponent} from '../../color/color-panel/color-picker-content/color-picker-content.component';
+import {ColorPickerItemComponent} from '../../color/color-panel/color-picker-item/color-picker-item.component';
 import { GridPanelComponent } from './grid-panel.component';
 
+// tslint:disable:no-string-literal
 describe('GridPanelComponent', () => {
   let component: GridPanelComponent;
   let fixture: ComponentFixture<GridPanelComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GridPanelComponent ]
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        MaterialModule,
+      ],
+      declarations: [
+        GridPanelComponent,
+        ColorPanelComponent,
+        ColorPickerItemComponent,
+        ColorPickerContentComponent,
+      ],
     })
     .compileComponents();
   }));
@@ -21,5 +37,21 @@ describe('GridPanelComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it ('onSquareSizeChange should call the pacthValue and handleGrid methods', () => {
+    const spyOnHandle = spyOn<any>(component['service'], 'handleGrid');
+    const spyOnPatch = spyOn<any>(component['gridForm'], 'patchValue');
+    component['onSquareSizeChange']();
+    expect(spyOnHandle).toHaveBeenCalled();
+    expect(spyOnPatch).toHaveBeenCalled();
+  });
+
+  it ('onOpacityChange should call the patchValue and handleGrid methods', () => {
+    const spyOnHandle = spyOn<any>(component['service'], 'handleGrid');
+    const spyOnPatch = spyOn<any>(component['gridForm'], 'patchValue');
+    component['onSquareSizeChange']();
+    expect(spyOnHandle).toHaveBeenCalled();
+    expect(spyOnPatch).toHaveBeenCalled();
   });
 });
--- client/src/app/tool/grid/grid.service.spec.ts
+++ client/src/app/tool/grid/grid.service.spec.ts
@@ -2,11 +2,44 @@ import { TestBed } from '@angular/core/testing';
 
 import { GridService } from './grid.service';
 
-describe('GridService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+enum gridKeys {
+  G = 'g',
+  plus = '+',
+  minus = '-',
+}
+
+// tslint:disable:no-string-literal
+fdescribe('GridService', () => {
+  let service: GridService;
+
+  beforeEach(() => {
+      TestBed.configureTestingModule({});
+      service = TestBed.get(GridService);
+      const testSVGStructure = {
+        root: document.createElementNS('http://www.w3.org/2000/svg', 'svg'),
+        defsZone: document.createElementNS('http://www.w3.org/2000/svg', 'svg:g') as SVGGElement,
+        drawZone: document.createElementNS('http://www.w3.org/2000/svg', 'svg:g') as SVGGElement,
+        temporaryZone: document.createElementNS('http://www.w3.org/2000/svg', 'svg:g') as SVGGElement,
+        endZone: document.createElementNS('http://www.w3.org/2000/svg', 'svg:g') as SVGGElement
+      };
+      testSVGStructure.root.appendChild(testSVGStructure.defsZone);
+      testSVGStructure.root.appendChild(testSVGStructure.drawZone);
+      testSVGStructure.root.appendChild(testSVGStructure.temporaryZone);
+      testSVGStructure.root.appendChild(testSVGStructure.endZone);
+
+      service['svg'].structure.root.setAttribute('width', '1000');
+      service['svg'].structure.root.setAttribute('height', '1000');
+    }
+  );
 
   it('should be created', () => {
-    const service: GridService = TestBed.get(GridService);
     expect(service).toBeTruthy();
+  });
+
+  it('keyEvHandler should invert the "active" attribute when called with g', () => {
+    const expected = false;
+    service.active = expected;
+    service.keyEvHandler(gridKeys.G);
+    expect(service.active).not.toEqual(expected);
   });
 });
