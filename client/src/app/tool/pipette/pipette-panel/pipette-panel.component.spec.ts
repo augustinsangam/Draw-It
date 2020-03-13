8cc1b30558b5596582059be853644e72ccc4a88e
Mathurin <mathurin.chritin@polymtl.ca>
2020-03-13 11:33:56 -0400

    début tests pipette

--- client/src/app/tool/pipette/pipette-panel/pipette-panel.component.spec.ts
+++ client/src/app/tool/pipette/pipette-panel/pipette-panel.component.spec.ts
@@ -1,5 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MaterialModule} from '../../../material.module';
+import {PanelComponent} from '../../../panel/panel.component';
+import {ColorPanelComponent} from '../../color/color-panel/color-panel.component';
+import {ColorPickerContentComponent} from '../../color/color-panel/color-picker-content/color-picker-content.component';
+import {ColorPickerItemComponent} from '../../color/color-panel/color-picker-item/color-picker-item.component';
 import { PipettePanelComponent } from './pipette-panel.component';
 
 describe('PipettePanelComponent', () => {
@@ -8,7 +14,18 @@ describe('PipettePanelComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PipettePanelComponent ]
+      declarations: [
+        PipettePanelComponent,
+        PanelComponent,
+        ColorPanelComponent,
+        ColorPickerItemComponent,
+        ColorPickerContentComponent,
+      ],
+      imports: [
+        MaterialModule,
+        ReactiveFormsModule,
+        FormsModule,
+      ]
     })
     .compileComponents();
   }));
