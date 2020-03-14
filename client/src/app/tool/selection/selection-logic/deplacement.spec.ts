c6ed2ee2b626a2f2767a3a35a3f65b03a5954657
Eya-Tom Augustin SANGAM <eya-tom-augustin.sangam@polymtl.ca>
2020-03-13 23:19:22 -0400

    Refactoring Selection

--- client/src/app/tool/selection/selection-logic/deplacement.spec.ts
+++ client/src/app/tool/selection/selection-logic/deplacement.spec.ts
@@ -1,8 +1,18 @@
+import { Renderer2 } from '@angular/core';
+import { async, TestBed } from '@angular/core/testing';
 import { Deplacement } from './deplacement';
 
-// TODO : Ask the chargé de lab
 // tslint:disable: no-magic-numbers
 describe('DeplacementUtil', () => {
+
+  let renderer: Renderer2;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      providers: [ Renderer2 ]
+    });
+    renderer = TestBed.get(Renderer2);
+  }));
 
   it('#getTransformTranslate works well', () => {
     const element = document.createElementNS(
@@ -14,10 +24,10 @@ describe('DeplacementUtil', () => {
   it('#translate works well', () => {
     const element = document.createElementNS(
       'http://www.w3.org/2000/svg', 'svg:rect');
-    Deplacement.translate(element, -10, 2);
+    Deplacement.translate(element, -10, 2, renderer);
     expect(Deplacement.getTransformTranslate(element))
       .toEqual([-10, 2]);
-    Deplacement.translate(element, -10, 2);
+    Deplacement.translate(element, -10, 2, renderer);
     expect(Deplacement.getTransformTranslate(element))
       .toEqual([-20, 4]);
   });
@@ -29,7 +39,7 @@ describe('DeplacementUtil', () => {
       elements[index] = document.createElementNS(
         'http://www.w3.org/2000/svg', 'svg:rect');
     }
-    Deplacement.translateAll(elements, -10, 2);
+    Deplacement.translateAll(elements, -10, 2, renderer);
     for (let index = 0; index < nElement; index++) {
       expect(Deplacement.getTransformTranslate(elements[index]))
       .toEqual([-10, 2]);
