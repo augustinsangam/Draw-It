d0fdfab941dc7fa391c7fa77bd8908cf3cf9b60d
Samba Bal <samba_bousso.bal@polymtl.ca>
2020-03-16 02:14:02 -0400

    Debut de test avec filter service

--- client/src/app/tool/drawing-instruments/brush/filter.service.spec.ts
+++ client/src/app/tool/drawing-instruments/brush/filter.service.spec.ts
@@ -3,7 +3,17 @@ import { TestBed } from '@angular/core/testing';
 import { FilterService } from './filter.service';
 import { Renderer2 } from '@angular/core';
 
-describe('FilterService', () => {
+fdescribe('FilterService', () => {
+  const renderer = {
+    setAttribute: ( element: SVGElement, attribute: string, value: string ) => {
+      element.setAttribute( attribute, value);
+    },
+    createElement: (value: string ) => {
+      document.createElement(value);
+    }
+  } as unknown as Renderer2;
+
+  
   beforeEach(() => TestBed.configureTestingModule({
     providers: [
       Renderer2
@@ -15,11 +25,9 @@ describe('FilterService', () => {
     expect(service).toBeTruthy();
   });
 
-  // it('generateBrushFilters should return a defZone with 5 elements', () => {
-  //   const service: FilterService = TestBed.get(FilterService);
-  //   // const renderer = {} as unknown as Renderer2;
-  //   const renderer: Renderer2 = TestBed.get(Renderer2);
-  //   const defZone: SVGDefsElement = service.generateBrushFilters(renderer);
-  //   expect(defZone.children.length).toEqual(6);
-  // });
+  it('generateBrushFilters should return a defZone with 5 elements', () => {
+    const service: FilterService = TestBed.get(FilterService);
+    const defZone: SVGDefsElement = service.generateBrushFilters(renderer);
+    expect(defZone.children.length).toEqual(6);
+  });
 });
