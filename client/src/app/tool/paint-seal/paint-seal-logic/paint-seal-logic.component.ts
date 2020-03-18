14d2b1390bf06fcaf8d046665069d78744bcab11
Eya-Tom Augustin SANGAM <eya-tom-augustin.sangam@polymtl.ca>
2020-03-18 16:05:32 -0400

    Méthode utilisant un scanline mais pas très efficace

--- client/src/app/tool/paint-seal/paint-seal-logic/paint-seal-logic.component.ts
+++ client/src/app/tool/paint-seal/paint-seal-logic/paint-seal-logic.component.ts
@@ -2,14 +2,10 @@ import { Component, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 import { SvgToCanvasService } from 'src/app/svg-to-canvas/svg-to-canvas.service';
 import { ColorService } from '../../color/color.service';
 import { RGBAColor } from '../../color/rgba-color';
+import { Point } from '../../shape/common/point';
 import { ToolLogicDirective } from '../../tool-logic/tool-logic.directive';
-
-interface Point {
-  x: number;
-  y: number;
-}
-
 const MAX_RGBA = 255;
+
 @Component({
   selector: 'app-paint-seal-logic',
   template: ''
@@ -21,14 +17,12 @@ export class PaintSealLogicComponent
   private canvasContext: CanvasRenderingContext2D;
   private imageData: ImageData;
   private allListeners: (() => void)[] = [];
-  private stack: Point[];
 
   constructor(private readonly svgToCanvas: SvgToCanvasService,
               private renderer: Renderer2,
               private colorService: ColorService,
   ) {
     super();
-    this.stack = [];
   }
 
   ngOnInit(): void {
@@ -54,52 +48,80 @@ export class PaintSealLogicComponent
 
   private onMouseClick($event: MouseEvent): void {
     this.imageData = this.canvasContext.getImageData(0, 0, this.svgShape.width, this.svgShape.height);
-    const colorToReplace = this.getColor($event.offsetX, $event.offsetY);
-    this.fill($event.offsetX, $event.offsetY, colorToReplace);
+    const oldColor = this.getColor($event.offsetX, $event.offsetY);
+    this.fill($event.offsetX, $event.offsetY, oldColor);
     this.canvasContext.putImageData(this.imageData, 0, 0);
     this.renderer.appendChild(this.svgStructure.root.parentNode, this.canvas);
-    this.stack = [];
   }
 
-  private fill(startingX: number, startingY: number, colorToReplace: RGBAColor): void {
+  // tslint:disable-next-line: cyclomatic-complexity
+  private fill(startingX: number, startingY: number, oldColor: RGBAColor): void {
 
-    this.stack.push({
-      x: startingX,
-      y: startingY
-    });
+    const isOldColor: (x: number, y: number) => boolean = (x: number, y: number) => {
+      if (!this.isValidCoordinates(x, y)) {
+        return false;
+      }
+      return this.colorService.rgbaEqual(oldColor, this.getColor(x, y));
+    };
 
     const replaceColor = this.colorService.rgbaFromString(
       this.colorService.primaryColor
     );
 
-    while (this.stack.length !== 0) {
+    const h = this.svgShape.height;
+    const w = this.svgShape.width;
 
-      if (this.stack.length > 5000) {
+    let x1: number;
+    let spanAbove: boolean;
+    let spanBelow: boolean;
+
+    const stack: Point[] = [];
+
+    stack.push(new Point(startingX, startingY));
+
+    while (stack.length !== 0) {
+
+      if (stack.length > 100000) {
         break;
       }
 
-      const pointToVisit = this.stack.shift() as Point;
+      const currentPoint = stack.pop() as Point;
+      const [x, y] = [currentPoint.x, currentPoint.y];
 
-      const [x, y] = [pointToVisit.x, pointToVisit.y];
-
-      if (this.isValidCoordinates(x, y)
-        && this.colorService.rgbaEqual(colorToReplace, this.getColor(x, y))) {
-        this.replaceColor(x, y, replaceColor);
-        this.stack.push({ x: x + 1, y });
-        this.stack.push({ x: x - 1, y });
-        this.stack.push({ x, y: y + 1 });
-        this.stack.push({ x, y: y - 1 });
+      x1 = x;
+      while (x1 >= 0 && isOldColor(x1, y)) {
+        x1--;
       }
+
+      x1++;
+      spanAbove = spanBelow = false;
+      while (x1 < w && isOldColor(x1, y)) {
+        this.replaceColor(x1, y, replaceColor);
+        if (!spanAbove && y > 0 && isOldColor(x1, y - 1)) {
+          stack.push(new Point(x1, y - 1));
+          spanAbove = true;
+        } else if (spanAbove && y > 0 && isOldColor(x1, y - 1)) {
+          spanAbove = false;
+        }
+        if (!spanBelow && y < h - 1 && isOldColor(x1, y + 1)) {
+          stack.push(new Point(x1, y + 1));
+          spanBelow = true;
+        } else if (spanBelow && y < h - 1 && isOldColor(x1, y + 1)) {
+          spanBelow = false;
+        }
+        x1++;
+      }
+
     }
+
   }
 
-  private replaceColor(x: number, y: number, replaceColor: RGBAColor): void {
+  private replaceColor(x: number, y: number, newColor: RGBAColor): void {
     let position = ( y * this.svgShape.width + x ) * 4 ;
-    this.imageData.data[position++] = replaceColor.r;
-    this.imageData.data[position++] = replaceColor.g;
-    this.imageData.data[position++] = replaceColor.b;
-    this.imageData.data[position++] = MAX_RGBA;
-    // this.imageData.data[position++] = replaceColor.a * MAX_RGBA;
+    this.imageData.data[position++] = newColor.r;
+    this.imageData.data[position++] = newColor.g;
+    this.imageData.data[position++] = newColor.b;
+    this.imageData.data[position++] = newColor.a * MAX_RGBA;
   }
 
   private isValidCoordinates(x: number, y: number): boolean {
@@ -107,7 +129,7 @@ export class PaintSealLogicComponent
       && 0 <= y && y <= this.svgShape.height;
   }
 
-  getColor(x: number, y: number): RGBAColor {
+  private getColor(x: number, y: number): RGBAColor {
     const pixel = this.canvasContext.getImageData(x, y, 1, 1).data;
     return {
       r: pixel[0],
