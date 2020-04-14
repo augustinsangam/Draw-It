4ab2b491dd723143a171df4b829fda59e2e984c3
Issam Maghni <me@concati.me>
2020-04-14 00:41:47 -0400

    Ne pas afficher le PID du serveur car nodemon le fait

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,8 +5,6 @@ import { myContainer } from './inversify.config';
 import { main } from './main';
 import { Server } from './server';
 
-log.info(`PID is ${process.pid}`);
-
 const server = myContainer.get<Server>(TYPES.Server);
 const sigint = new Promise<void>((resolve) => {
 	process.on('SIGINT', () => resolve());
