b262b7bd299a034da86a7045bc0e41ad5f9a7fb9
Eya-Tom Augustin SANGAM <augustin.sangam@gmail.com>
2020-05-02 03:20:08 -0400

    Electron se lance

---
+++ client/main.ts
@@ -0,0 +1,41 @@
+// const { app, BrowserWindow } = require('electron')
+import { app, BrowserWindow } from 'electron';
+let win: BrowserWindow|null;
+
+const createWindow = () => {
+  // Create the browser window.
+  win = new BrowserWindow({
+    minWidth: 1300,
+    minHeight: 900,
+    backgroundColor: '#FFFFFF',
+  });
+
+  win.loadURL(`file://${__dirname}/dist/client/index.html`)
+
+  //// uncomment below to open the DevTools.
+  // win.webContents.openDevTools()
+
+  // Event when the window is closed.
+  win.on('closed', () => {
+    win = null;
+  });
+
+};
+
+// Create window on electron intialization
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+  // On macOS specific close process
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // macOS specific close process
+  if (win === null) {
+    createWindow();
+  }
+});
